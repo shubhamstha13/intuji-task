bacca453bf0f8e1180973cadc3f321b3e1766f71
Shubham Shrestha <sthaluffy85@gmail.com>
2024-12-31 16:59:15 +0545

    added realtime, forecast, airquality api

--- db.php
+++ db.php
@@ -9,9 +9,9 @@
 require "config.php";
 
  // Create connection
-$db_connect = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 // Check connection
-if ($db_connect->connect_error) {
-    die("Connection failed: " . $db_connect->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include 'db.php';
+require_once("db.php");
 header("Content-Type: application/json");
 
 $request = $_SERVER['REQUEST_URI'];
@@ -22,9 +22,33 @@ switch ($request) {
         }
         break;
 
+    case '/intuji/weather/generate':
+        if($method == 'POST'){
+            weatherGenerate();
+        }else{
+            echo json_encode(['message' => 'Invalid request method']);
+        }
+        break;
+
     case '/intuji/weather/realtime?location=' . $_GET['location']:
         if($method == 'GET'){
             weatherRealtime($_GET['location']);
+        }else{
+            echo json_encode(['message' => 'Invalid request method']);
+        }
+        break;
+
+    case '/intuji/weather/forecast?location=' . $_GET['location']:
+        if($method == 'GET'){
+            weatherForecast($_GET['location']);
+        }else{
+            echo json_encode(['message' => 'Invalid request method']);
+        }
+        break;
+  
+    case '/intuji/weather/airquality?location=' . $_GET['location']:
+        if($method == 'GET'){
+            airQuality($_GET['location']);
         }else{
             echo json_encode(['message' => 'Invalid request method']);
         }
@@ -35,9 +59,27 @@ switch ($request) {
         break;
 }
 
-function weatherRealtime($location){
-    $location_id = $db_connect->query("SELECT id FROM location WHERE name=$location");
+function weatherRealtime($conn,$location){
+    $location_id = $conn->query("SELECT id FROM location WHERE name=$location");
     $result = $conn->query("SELECT locations.name as location, temperature , `condition`, humidity ,wind_speed from weather_realtime JOIN locations where locations.id = $location_id");
     $data = $result->fetch_assoc();
     echo json_encode($data);
 }  
+
+function weatherForecast($conn,$location){
+    $location_id = $conn->query("SELECT id FROM location WHERE name=$location");
+    $result = $conn->query("SELECT locations.name as location, `date` , max_temp as maximum_temperature ,min_temp as minimum_temperature , `condition` from weather_forecast JOIN locations where locations.id = $location_id AND date > date(now() - INTERVAL 3 day)");
+    $data = $result->fetch_assoc();
+    echo json_encode($data);
+}  
+
+function airQuality($conn,$location){
+    $location_id = $conn->query("SELECT id FROM location WHERE name=$location");
+    $result = $conn->query("SELECT locations.name as location, aqi , description from weather_forecast JOIN locations where locations.id = $location_id");
+    $data = $result->fetch_assoc();
+    echo json_encode($data);
+}  
+
+function weatherGenerate(){
+    
+}
